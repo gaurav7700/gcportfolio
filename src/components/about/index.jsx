141585cc523138cd83e5d9ae873fa7d2992e8ed1
GAURAVC <gaurav27c@gmail.com>
2022-01-30 22:19:13 +1100

    My first commit

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -36,7 +36,7 @@ const index = () => {
               {/* End personal info */}
 
               <div className="col-12 mt-1">
-                <a className="button" href="img/cv.webp" download>
+                <a className="button" href="https://drive.google.com/file/d/1QIIDiMKrnyykwjq5VDOo08d3FvRxuezw/view?usp=sharing" download>
                   <span className="button-text">Download CV</span>
                   <span className="button-icon fa fa-download"></span>
                 </a>
